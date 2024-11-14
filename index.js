0c51cd5548d4cf5dbe3af8734ff3faf427fa9f46
Aji Surya Ahmadi <ajisuryaahmadi@gmail.com>
2024-11-14 18:19:59 +0700

    remove socket

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const path = require('path');
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const app = require("./features/app");
-const socket = require("./features/socket");
-
 // api services
 const server = app.listen(process.env.API_PORT, async function () {
     return console.log(`Backend service running on port ${process.env.API_PORT}`);
