479bbe1fe8b36a74ce1c60e1c3423aa324f78e1b
Aji Surya Ahmadi <ajisuryaahmadi@gmail.com>
2024-11-25 10:55:18 +0700

    add new global attributes

--- features/list.js
+++ features/list.js
@@ -1,8 +1,10 @@
 // Global attributes for model
 const membershipAttributes = ['id', 'venue_id', 'name', 'description', 'price', 'created_at', 'updated_at'];
 const venueAttributes = ['uuid', 'name', 'address', 'contact_number', 'longitude', 'latitude', 'created_at', 'updated_at'];
+const levelAttributes = ['id_account', 'id_venue', 'level_account'];
 
 module.exports = {
     membershipAttributes,
-    venueAttributes
+    venueAttributes,
+    levelAttributes
 }
