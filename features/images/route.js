b5732d5d0eac5cfa15ede785022dd88671c8ccf2
Aji Surya Ahmadi <ajisuryaahmadi@gmail.com>
2024-12-05 01:06:42 +0700

    change route name

--- features/images/route.js
+++ features/images/route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const token = require('../../tools/token');
 const imageController = require('./controller');
 // Routes
-router.get('/image/:fileName', token.jwtHandler, (req, res) => imageController.serveImage(req, res));
+router.get('/:fileName', token.jwtHandler, (req, res) => imageController.serveImage(req, res));
 
 module.exports = {
     "name": imageController.prefix,
