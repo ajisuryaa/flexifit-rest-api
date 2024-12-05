1961d3867c8124086af6dcf0072da9dc988d7aec
Aji Surya Ahmadi <ajisuryaahmadi@gmail.com>
2024-12-06 03:26:05 +0700

    update condition

--- features/venues/controller.js
+++ features/venues/controller.js
@@ -82,7 +82,7 @@ class VenueController {
                 where: {
                     deleted_at: null
                 },
-                attributes: ['uuid', 'name', 'address', 'contact_number', 'longitude', 'latitude'],
+                attributes: ['uuid', 'name', 'address', 'contact_number', 'longitude', 'latitude', 'created_at', 'updated_at'],
             });
 
             if (venues.length > 0) {
@@ -92,6 +92,7 @@ class VenueController {
                     data: venues
                 });
             }
+
             return res.status(200).json({
                 success: true,
                 message: 'Venues data is empty.'
